c247d7cc2809510275242bf8c06ab5280e7144bc
144380980+cyrusrogers@users.noreply.github.com <nderitcyrus@gmail.com>
2025-02-11 12:37:00 +0300

    create getHumanChoice function

--- rock.js
+++ rock.js
@@ -1,23 +1,43 @@
 console.log("Hello World!")
 function getComputerChoice() {
-  let computerChoice = Math.round(Math.random()*100)/100
-  if (computerChoice < 0.34) {
+  let computerMove = Math.round(Math.random()*100)/100
+  if (computerMove < 0.34) {
     return "rock";
-  } else if (computerChoice >= 0.34 && computerChoice < 0.67) {
+  } else if (computerMove >= 0.34 && computerMove < 0.67) {
     return "paper";
   } else {
      return "scissors"
   }
  
 }
-
-/*console.log(getComputerChoice())
-counter = 0
+let computerChoice = getComputerChoice();
+//console.log(computerChoice)
+/*counter = 0
 while (counter < 10) {
    console.log(getComputerChoice());
    counter++;
 }*/
 
 function getHumanChoice() {
+  let options = ["rock","paper","scissors"]
+  let humanMove = prompt("Pick your move 'rock','paper'/'scissors': ").toLowerCase();
+  if (options.includes(humanMove)) {
+    return humanMove;
+  } else {
+    console.log("Invalid move!")
+  }
+}
+
+let humanChoice = getHumanChoice ();
+//console.log(humanChoice)
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+  console.log(`humanMove: ${humanChoice}`);
+  console.log(`computerMove: ${computerChoice}`);
   
 }
+console.log(playRound(humanChoice,computerChoice));
